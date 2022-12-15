38b9c731ef725120545e0ec079177d3009999e37
Aingthawawn K <109336369+aingthawan@users.noreply.github.com>
2022-12-15 13:35:14 +0700

    Create work_1.java

---
+++ work_1.java
@@ -0,0 +1,14 @@
+/*
+My first java line
+Aingthawan K.
+2017-01-10
+*/
+
+/**
+ * work_1
+ */
+public class work_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World, Aing");
+    }
+}
