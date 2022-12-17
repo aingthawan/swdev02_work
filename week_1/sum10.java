b4397cfb01a25a82fb26870383373684246122ab
Aingthawawn K <109336369+aingthawan@users.noreply.github.com>
2022-12-17 16:04:01 +0700

    updated code

--- week_1/sum10.java
+++ week_1/sum10.java
@@ -8,7 +8,7 @@ public class sum10 {
   public static void main(String[] args) throws Exception {
 
     // Open the txt input file    +   new scanner
-    File file = new File("input_sum10test.txt");
+    File file = new File("input_sum10test2.txt");
     Scanner scanner = new Scanner(file);
 
     // Read the first libe of txt file for matrix size
@@ -27,7 +27,9 @@ public class sum10 {
     // Close scanner
     scanner.close();
 
-
+    int r_sum;
+    int d_sum;
+    int tenCount = 0;
 
     for (int i = 0; i < matrixSize; i++){
       for (int j = 0; j < matrixSize; j++){
@@ -35,6 +37,36 @@ public class sum10 {
       }
       System.out.println();
     }
+    System.out.println();
+    System.out.println("=======================================");
+    System.out.println();
+
+    for (int i = 0; i < matrixSize; i++){
+      for (int j = 0; j < matrixSize; j++){
+        System.out.print(matrix[i][j] + " ");
+        
+        r_sum = 0;
+        d_sum = 0;
+
+        System.out.print(" Go R : ");
+        for (int k = j+1; k < matrixSize; k++){
+          //System.out.print(matrix[i][k]);
+          r_sum = r_sum + matrix[i][k];
+        }
+        System.out.print(r_sum);
+
+        System.out.print(" Go D : ");
+        for (int l = i+1; l < matrixSize; l++){
+          //System.out.print(matrix[l][i]);
+          d_sum = d_sum + matrix[l][i];
+        }
+        System.out.print(d_sum);
+
+
+        System.out.println();
+      }
+    }
+
 
   }
 }
