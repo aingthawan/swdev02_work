a8a04eae4f682737b0c67b3b8bc2808d1239a0d1
Aingthawawn K <109336369+aingthawan@users.noreply.github.com>
2022-12-18 19:38:43 +0700

    Update ex7.java

--- week_1/ex7.java
+++ week_1/ex7.java
@@ -36,18 +36,63 @@ public class ex7 {
 
         // Create a function to find x1 and x2 where c1 = x1 + x2
         int i, j, k, l;
+        int p, q, r, s;
+        int an;
         for(i = 0; i < n; i++) {
             for(j = 0; j < n; j++) {
-                if((A[i] + A[j] == c1) && (i != j)) {
+                if((A[i] + A[j] == c1) && !(seenNumbers.contains(A[i])) && !(seenNumbers.contains(A[j]))) {
                     x[0] = A[i];
                     x[1] = A[j];
                     seenNumbers.add(x[0]);
                     seenNumbers.add(x[1]);
+                    System.out.println(" ");
                     System.out.println("x1 = " + x[0] + " x2 = " + x[1]);
 
+                    for(k = 0; k < n; k++){
+                        for(l = 0; l < n; l++){
+                            if((A[l] == A[k] + x[0]) && !(seenNumbers.contains(A[k])) && !(seenNumbers.contains(A[l]))){
+                                x[2] = A[k];
+                                x[3] = A[l];
+                                seenNumbers.add(x[2]);
+                                seenNumbers.add(x[3]);
+                                System.out.println("x3 = " + x[2] + " x4 = " + x[3]);
+
+                                for(p = 0; p < n; p++){
+                                    for(q = 0; q < n; q++){
+                                        if((A[q] == c2 + A[p]) && !(seenNumbers.contains(A[p])) && !(seenNumbers.contains(A[q]))){
+                                            x[5] = A[p];
+                                            x[7] = A[q];
+                                            seenNumbers.add(x[5]);
+                                            seenNumbers.add(x[7]);
+                                            System.out.println("x6 = " + x[5] + " x8 = " + x[7]);
+
+                                            for(r = 0; r < n; r++){
+                                                for(s = 0; s < n; s++){
+                                                    if((A[s] == A[r] + A[5])){
+                                                        x[4] = A[r];
+                                                        x[6] = A[s];
+                                                        seenNumbers.add(x[4]);
+                                                        seenNumbers.add(x[6]);
+                                                        System.out.println("x5 = " + x[4] + " x7 = " + x[6]);
+
+                                                        // for(an = 0; an < 8; an++){
+                                                        //     System.out.println(x[an]);
+                                                        // }
+                                                    }
+                                                }
+                                            }
+
+                                        }
+                                    }
+                                }
+
+                            }
+                        }
+                    }
                     
                 }
             }
+            seenNumbers = new HashSet<>();
         }    
     }
 }
