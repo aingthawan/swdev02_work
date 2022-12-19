ed54d4931eb06f6b0e51646c16ab1b7838c12c80
Aingthawawn K <109336369+aingthawan@users.noreply.github.com>
2022-12-20 01:08:14 +0700

    added 3 unit test for 3sum problem

---
+++ week_1/ex5/Week1_3SumProblemTest.java
@@ -0,0 +1,32 @@
+package ex5;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.Test;
+
+public class Week1_3SumProblemTest {
+    @Test
+    public void testSum0() {
+      int[] arr0 = {0, -1, 2, -3, 1, -2};                              // Test Input 1 : Correct Answer
+      int[][] expected0 = {{-3, 1, 2}, {-2, 0, 2}, {-1, 0, 1} };
+      int[][] result = week1_3sumProblem.sum(arr0);
+      assertArrayEquals(expected0, result);
+    }
+
+    @Test
+    public void testSum1() {
+      int[] arr1 = {-1, 0, 1, 2, -1, -4};
+      int[][] expected1 = {{-1, -1, 2}, {-1, 0, 1}, {-1, 0, 1} };      // Test Input 2 : Correct Answer
+      //int[][] expected1 = {{-1, -1, 2}, {-1, 0, 1}, {0, 0, 0} };     
+      int[][] result = week1_3sumProblem.sum(arr1);
+      assertArrayEquals(expected1, result);
+    }
+
+    @Test
+    public void testSum2() {                                           // Test Empty Input : Return Null Correct Answer
+      int[] arr2 = {1, 1, 1, 1, 1, 1};
+      int[][] result = week1_3sumProblem.sum(arr2);
+      assertArrayEquals(null, result);
+    }
+    
+}
