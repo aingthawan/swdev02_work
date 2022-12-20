adce584edb43e68fcc74ee58a31d070380ee96ec
Aingthawawn K <109336369+aingthawan@users.noreply.github.com>
2022-12-20 09:12:20 +0700

    try web scraping

---
+++ webScraping_test/example.py
@@ -0,0 +1,17 @@
+# Aing K.
+# Test Web Scraping with Python
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.borntodev.com/online-course/"
+web_data = requests.get(url)
+
+soup = BeautifulSoup(web_data.text, "html.parser")
+find_word = soup.find_all("h2", {"class": "woocommerce-loop-product__title"})
+
+for i in find_word:
+    i = str(i).split('<h2 class="woocommerce-loop-product__title">')[1]
+    i = str(i).split('</h2>')[0]
+    print(i)
+
