4ad7858dfb6acb49be398092eb939438802acf37
Aingthawawn K <109336369+aingthawan@users.noreply.github.com>
2023-02-11 20:38:21 +0700

    ELT system prototype

--- project/src/ELT_transform.py
+++ project/src/ELT_transform.py
@@ -1,18 +1,19 @@
+# a file to transform the raw data into the main database
+# standby and check for new data in the raw database every 5 seconds
+# if new data is found, transform it and add it to the main database
+# then delete the row from the raw database
+# 
+# Dev : AingKK.
+
 from linkChecker import *
 from dataPipeline import *
 from cleanRawText import *
 from singleScrape import *
 
-import sqlite3
 from urllib.parse import urlparse
+import sqlite3
+import time
 
-directory = "project\src\database\\"
-raw_database = directory + "database_elt_raw_test3.db"
-main_database = directory + "database_elt_test1.db"
-
-
-# ================================================================================================
-# ================================================================================================
 
 def word_frequency_dict(words_list):
     """Turn list of words into dictionary with word as key and frequency as value"""
@@ -23,9 +24,6 @@ def word_frequency_dict(words_list):
         else:
             frequency_dict[word] = 1
     return frequency_dict    
-
-# ================================================================================================
-# ================================================================================================
 
 class raw_database:
     """class for getting the raw content from the database and remove"""
@@ -55,16 +53,11 @@ class raw_database:
         self.conn.commit()
         self.conn.close()
 
-
-
-class main_databse:
+class main_database:
     """class for processing the raw content and insert into the database"""
 
     def __init__(self, main_database):
         """initialize the database"""
-        self.conn = sqlite3.connect(main_database)
-        self.cur = self.conn.cursor()
-
         self.tc = TextCleaners()
         self.ps = pageScrapers()
         self.dp = dataPipelines(main_database)
@@ -81,21 +74,27 @@ class main_databse:
         """update the link into the database"""
         # Clean raw content
         clean_content = self.tc.clean(raw_content)
-        all_backlink_list = self.ps.scrape_all_urls(clean_content)
+        clean_content_dict = self.word_frequency_dict(clean_content)
+        
+        all_backlink_list = self.ps.scrape_all_urls(raw_content)
+        
+        url_domain = self.get_domain(url)
         # get a list of unique domain from all backlinks
         all_backlink_domain_list = []
         for link in all_backlink_list:
-            if self.get_domain(link) not in all_backlink_domain_list:
-                all_backlink_domain_list.append(self.get_domain(link))
+            link_domain = self.get_domain(link)
+            if (self.get_domain(link) not in all_backlink_domain_list) and (link_domain != url_domain):
+                all_backlink_domain_list.append(link_domain)
         
         new_id = self.dp.getUniqueID()
-
+    
         # updating reference domain
         self.dp.updateReferenceDomain(all_backlink_domain_list)
         # update webData
-        self.dp.updateWebData(new_id, url, clean_content)
+        # def updateWebData(self, web_id, url, all_words, ref_to):
+        self.dp.updateWebData(new_id, url, clean_content_dict, all_backlink_domain_list)
         # update invertedIndex
-        self.dp.updateInvertedIndex(new_id, clean_content)
+        self.dp.updateInvertedIndexing(new_id, clean_content)
 
 
     def removeData(self, url):
@@ -104,3 +103,51 @@ class main_databse:
         self.dp.removeWebData(temp_datarow['URL'])
         self.dp.uncountRef(temp_datarow['Ref_To'])
         self.dp.removeInvertedIndex(temp_datarow['Web_ID'], temp_datarow['All_Word'])
+        
+    def word_frequency_dict(self, words_list):
+        """Turn list of words into dictionary with word as key and frequency as value"""
+        frequency_dict = {}
+        for word in words_list:
+            if word in frequency_dict:
+                frequency_dict[word] += 1
+            else:
+                frequency_dict[word] = 1
+        return frequency_dict
+
+def data_processing():
+    
+    directory = "project\database\\"
+    raw_dir = directory + "database_elt_raw_test3.db"
+    main_dir = directory + "database_elt_test1.db"
+    
+    rawd = raw_database(raw_dir)
+    mdb = main_database(main_dir)
+    
+    while True:
+        
+        row_temp = rawd.get_row()
+        
+        if row_temp is None:
+            print("Table is empty. Waiting for data...")
+            time.sleep(5)
+            continue
+
+        else:
+            print("Processing data...")
+            url = row_temp[0]
+            raw = row_temp[1]
+            mdb.updateLink(url, raw)
+            rawd.delete_row(url)
+        
+        
+if __name__ == "__main__":    
+    try:
+        data_processing()
+    except KeyboardInterrupt:
+        print("Exiting program")
+
+
+#  _._     _,-'""`-._
+# (,-.`._,'(       |\`-/|
+#     `-.-' \ )-`( , o o)  < Katze is cat in German
+#           `-    \`_`"'-        
